b345c56efa232d6cdc5f8843046b134474192b54
Peter Zhang <pzmosquito@gmail.com>
2019-03-12 09:56:35 -0700

    add observable route objects in routerStore for easy access

--- package/src/RouterStore.js
+++ package/src/RouterStore.js
@@ -7,6 +7,11 @@ export default class RouterStore {
     route = null;
     previousRoute = null;
 
+    // observable route objects
+    @observable.ref
+    obsRoute = null;
+    obsPreviousRoute = null;
+
     // reference of router instance
     router = null;
 
@@ -15,23 +20,26 @@ export default class RouterStore {
 
     // route component to activate for route nodes
     @observable.shallow
-    routeNodePath = new Map();
+    _routeNodePath = new Map();
 
 
-    init(router, routes) {
+    _init(router, routes) {
         this.router = router;
         this.routes = routes;
 
         this.router.subscribe(routeObj => {
-            this.routeUpdated(routeObj);
+            this._routeUpdated(routeObj);
         });
     }
 
     // handle route update
     @action
-    routeUpdated(routeObj) {
+    _routeUpdated(routeObj) {
         this.route = routeObj.route;
         this.previousRoute = routeObj.previousRoute;
+
+        this.obsRoute = routeObj.route;
+        this.obsPreviousRoute = routeObj.previousRoute;
 
         const {intersection, toActivate} = transitionPath(this.route, this.previousRoute);
         const updatedRoutes = [intersection].concat(toActivate);
@@ -39,22 +47,22 @@ export default class RouterStore {
         for (let i = 0; i < updatedRoutes.length - 1; i++) {
             const currRoute = updatedRoutes[i];
             const nextRoute = updatedRoutes[i + 1];
-            const nextRouteObj = this.getRoute(this.routes, nextRoute);
+            const nextRouteObj = this.getRoute(nextRoute);
 
-            this.routeNodePath.set(currRoute, Object.assign({}, nextRouteObj));
+            this._routeNodePath.set(currRoute, Object.assign({}, nextRouteObj));
         }
     }
 
     // get route object for a given route name.
-    getRoute(routes, routeName, parentRouteName) {
+    getRoute(routeName, routes = this.routes, parentRouteName = "") {
         for (const route of routes) {
-            const currentRouteName = parentRouteName ? `${parentRouteName}.${route.name}` : route.name;
+            const currentRouteName = parentRouteName === "" ? route.name : `${parentRouteName}.${route.name}`;
 
             if (routeName === currentRouteName) {
                 return route;
             }
             if (Object.prototype.hasOwnProperty.call(route, "children")) {
-                const routeFound = this.getRoute(route.children, routeName, currentRouteName);
+                const routeFound = this.getRoute(routeName, route.children, currentRouteName);
                 if (routeFound) {
                     return routeFound;
                 }
--- package/src/index.js
+++ package/src/index.js
@@ -7,14 +7,14 @@ const routerStore = new RouterStore();
 
 
 const routerApp = (router, routes, WrappedComponent) => {
-    routerStore.init(router, routes);
+    routerStore._init(router, routes);
 
     return WrappedComponent;
 };
 
 
 const RouteComponent = observer(({routeNodeName}) => {
-    const routeObj = routerStore.routeNodePath.get(routeNodeName);
+    const routeObj = routerStore._routeNodePath.get(routeNodeName);
     const Component = routeObj.component;
 
     return <Component />;
