e8ae6674b5ee1d8eb2132549be2c13afd7b0c281
Peter Zhang <pzmosquito@gmail.com>
2019-07-31 10:32:43 -0700

    fix some types

--- package/src/index.d.ts
+++ package/src/index.d.ts
@@ -3,18 +3,6 @@ import { Router, Route } from "router5";
 import { MiddlewareFactory } from "router5/types/types/router";
 
 
-// mr-router5's Route object, different from rotuer5's Route
-declare interface RouteObj {
-    name: string,
-    path: string,
-    component: React.ReactNode,
-    children?: RouteObj[],
-    loader?: Function,
-    preloader?: Function,
-    postloader?: Function
-}
-
-
 declare class RouterStore {
     route: Route | null;
     previousRoute: Route | null;
@@ -23,22 +11,34 @@ declare class RouterStore {
     obsPreviousRoute: Route | null;
 
     router: Router | null;
-    routes: RouteObj[] | null;
+    routes: RouteDef[] | null;
 
     getRoute(
         routeName: string,
-        routes: RouteObj[],
+        routes: RouteDef[],
         parentRouteName: string
-    ): RouteObj | null;
+    ): RouteDef | null;
+}
+
+
+// mr-router5's Route definition, different from rotuer5's Route object
+export declare interface RouteDef {
+    name: string,
+    path: string,
+    component: React.ComponentType<object>,
+    children?: RouteDef[],
+    loader?: Function,
+    preloader?: Function,
+    postloader?: Function
 }
 
 export declare const routerStore: RouterStore;
 
 export declare const routerApp: (
     router: Router,
-    routes: RouteObj[],
-    WrappedComponent: React.ReactNode
-) => React.ReactNode;
+    routes: RouteDef[],
+    WrappedComponent: React.ComponentType<object>
+) => typeof WrappedComponent;
 
 export declare const RouteComponent: React.FunctionComponent<{ routeNodeName: string }>;
 
